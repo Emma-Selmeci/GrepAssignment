0c80eaa587b29f1bf3c440bedfe61ae12a49299a
Emma Selmeci <mtyselmeci@gmail.com>
2025-02-22 09:52:03 +0100

    first commit

---
+++ src/eselmeci_grep_assignment/Grep.java
@@ -0,0 +1,11 @@
+package eselmeci_grep_assignment;
+
+
+
+public class Grep {
+    private Grep() {} //Grep is singleton
+
+    public static void main(String[] args) {
+        System.out.println("Grep");
+    }
+}
---
+++ src/eselmeci_grep_assignment/GrepTest.java
@@ -0,0 +1,7 @@
+package eselmeci_grep_assignment;
+
+public class GrepTest {
+    public static void main(String[] args) {
+        System.out.println("Greptest");
+    }
+}
