88e165aee26b2c04b0be3a89e56196940d2ebabe
Emma Selmeci <mtyselmeci@gmail.com>
2025-02-23 19:36:59 +0100

    Parser finished, tests written

--- src/eselmeci_grep_assignment/GrepTest.java
+++ src/eselmeci_grep_assignment/GrepTest.java
@@ -3,19 +3,28 @@ package eselmeci_grep_assignment;
 public class GrepTest {
     static String s = "";
     static Exception error = null;
+    static InputParams p = null;
+    static int testNum = 1;
 
     public static void main(String[] args) {
 
-        //I'm pretty sure there's a MUCH better way of doing this but I don't have the technical know-how
-        int testNum = 1;
-        String testString = "";
-        System.out.printf("Test %d, input \"%s\"\n", testNum++, testString);
+        //I'm pretty sure there's a MUCH better way of doing this, but I don't have the technical know-how
+
+        String testString;
+
+        /*#########################*/
+        /*####Testing setFlags()###*/
+        /*#########################*/
+
+        testString = "-a";
+        System.out.printf("setFlags() test %d, input \"%s\"\n", testNum++, testString);
+        p = new InputParams();
         try {
-            s = InputParser.parseInput("");
-        } catch (Exception e) {
+            InputParser.setFlags(testString,p);
+        } catch (InvalidFlagException e) {
             error = e;
         } finally {
-            if (error instanceof InvalidInputLayoutException) {
+            if (error instanceof InvalidFlagException) {
                 System.out.println("Test passed");
             } else {
                 System.out.println("Test failed");
@@ -24,11 +33,186 @@ public class GrepTest {
 
         reset();
 
+        testString = "-wc";
+        System.out.printf("setFlags() test %d, input \"%s\"\n", testNum++, testString);
+        p = new InputParams();
+        try {
+            InputParser.setFlags(testString,p);
+        } catch (InvalidFlagException e) {
+            error = e;
+        } finally {
+            if (error == null && !p.x && !p.n && p.c && !p.i && p.w && !p.v) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        testString = "-wcww";
+        System.out.printf("setFlags() test %d, input \"%s\"\n", testNum++, testString);
+        p = new InputParams();
+        try {
+            InputParser.setFlags(testString,p);
+        } catch (InvalidFlagException e) {
+            error = e;
+        } finally {
+            if (error == null && !p.x && !p.n && p.c && !p.i && p.w && !p.v) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        /*#########################*/
+        /*###Testing parseInput()##*/
+        /*#########################*/
+
+        testNum = 1;
+        testString = "";
+        System.out.printf("parseInput() test %d, input \"%s\"\n", testNum++, testString);
+        try {
+            s = InputParser.parseInput(testString);
+            p = InputParser.getParams();
+        } catch (Exception e) {
+            error = e;
+        } finally {
+            if (error instanceof InvalidInputLayoutException && p == null) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        testString = "-";
+        System.out.printf("parseInput() test %d, input \"%s\"\n", testNum++, testString);
+        try {
+            s = InputParser.parseInput(testString);
+            p = InputParser.getParams();
+        } catch (Exception e) {
+            error = e;
+        } finally {
+            if (error == null && p.pattern.equals("-")) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        testString = "-3";
+        System.out.printf("parseInput() test %d, input \"%s\"\n", testNum++, testString);
+        try {
+            s = InputParser.parseInput(testString);
+            p = InputParser.getParams();
+        } catch (Exception e) {
+            error = e;
+        } finally {
+            if (error == null && p.num == 0 && p.pattern.equals("-3")) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        testString = "-3 -c";
+        System.out.printf("parseInput() test %d, input \"%s\"\n", testNum++, testString);
+        try {
+            s = InputParser.parseInput(testString);
+            p = InputParser.getParams();
+        } catch (Exception e) {
+            error = e;
+        } finally {
+            if (error == null && p.num == 3 && p.pattern.equals("-c")) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        testString = "-3 -c a";
+        System.out.printf("parseInput() test %d, input \"%s\"\n", testNum++, testString);
+        try {
+            s = InputParser.parseInput(testString);
+            p = InputParser.getParams();
+        } catch (Exception e) {
+            error = e;
+        } finally {
+            if (error == null && p.num == 3 && p.c && p.pattern.equals("a")) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        testString = "-3 -c -e a";
+        System.out.printf("parseInput() test %d, input \"%s\"\n", testNum++, testString);
+        try {
+            s = InputParser.parseInput(testString);
+            p = InputParser.getParams();
+        } catch (Exception e) {
+            error = e;
+        } finally {
+            if (error == null && p.num == 3 && p.c && p.pattern.equals("a")) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        testString = "-3 -c -e a foo.txt";
+        System.out.printf("parseInput() test %d, input \"%s\"\n", testNum++, testString);
+        try {
+            s = InputParser.parseInput(testString);
+            p = InputParser.getParams();
+        } catch (Exception e) {
+            error = e;
+        } finally {
+            if (error == null && p.num == 3 && p.c && p.pattern.equals("a") && p.fileNames.length == 1 && p.fileNames[0].equals("foo.txt")) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
+        reset();
+
+        testString = "a foo.txt bar.txt";
+        System.out.printf("parseInput() test %d, input \"%s\"\n", testNum++, testString);
+        try {
+            s = InputParser.parseInput(testString);
+            p = InputParser.getParams();
+        } catch (Exception e) {
+            error = e;
+        } finally {
+            if (error == null && p.num == 0 && p.pattern.equals("a") && p.fileNames.length == 2 && p.fileNames[0].equals("foo.txt") && p.fileNames[1].equals("bar.txt")) {
+                System.out.println("Test passed");
+            } else {
+                System.out.println("Test failed");
+            }
+        }
+
     }
 
     static void reset() {
         s = "";
-        error =null;
+        error = null;
+        p = null;
+        ++testNum;
         System.out.println();
     }
 }
--- src/eselmeci_grep_assignment/InputParser.java
+++ src/eselmeci_grep_assignment/InputParser.java
@@ -1,13 +1,31 @@
 package eselmeci_grep_assignment;
 
-
+/**
+ * The InputParser class parses the String input (from Grep class or GrepTest) and hands parameters to AnswerGenerator
+ */
 
 public class InputParser {
+    /**
+     * @param input - String to be processed. Expected format is according to assignment, excluding the word grep
+     *              The String is split by whitespaces, and as a result, the search pattern or file names cannot include whitespaces
+     * @return the output for the program, a possibly multi-line String or an empty String
+     * @throws InvalidInputLayoutException
+     * @throws NumberFormatException
+     * @throws InvalidFlagException
+     */
+    static InputParams params;
+
+    //For debugging purposes
+    static InputParams getParams() {
+        return params;
+    }
+
+
     static String parseInput(String input) throws
             InvalidInputLayoutException,
             NumberFormatException,
             InvalidFlagException {
-
+        params = null;
         input = input.trim();
         if(input.isEmpty()) throw new InvalidInputLayoutException("No pattern provided");
         String[] splitInput = input.split("\\s+");
@@ -15,30 +33,33 @@ public class InputParser {
         /*
         If we've reached this point, our splitInput has at least one element.
         If it's one element only, we immediately assume that this is a pattern and the user intends to use grep on the console.
-        This means that even if the user only provides a string like "-c", we assume that this is the pattern.
+        This means that even if the user only provides a string like "-c" or "foo.txt", we assume that this is the pattern.
          */
 
-        InputParams params = new InputParams();
+        params = new InputParams();
         int counter = 0;
 
         //Checking for num input
-        if(splitInput[counter].charAt(0) == '-' && splitInput[counter].charAt(1) >= '0' && splitInput[counter].charAt(1) <= '9') {
-            params.num = Integer.parseInt(splitInput[counter].substring(1));
+        if(counter < splitInput.length-1 && splitInput[counter].length() > 1 && splitInput[counter].charAt(0) == '-' && splitInput[counter].charAt(1) >= '0' && splitInput[counter].charAt(1) <= '9') {
+            params.num = Integer.parseInt(splitInput[counter].substring(1)); //Possibly throwing a NumberFormatException
             ++counter;
         }
 
-        if(splitInput[counter].charAt(0) == '-') {
-            if(splitInput[counter].charAt(1) != 'e') {
-                setFlags(splitInput[counter],params);
+        //Checking for flags
+        if(counter < splitInput.length-1 && splitInput[counter].length() > 1 && splitInput[counter].charAt(0) == '-') {
+            if(splitInput[counter].length() == 2 && splitInput[counter].charAt(1) != 'e') {
+                setFlags(splitInput[counter],params); //Extracting the flags (possibly getting an InvalidFlagException)
                 ++counter;
-                if(splitInput[counter].charAt(0) == '-' && splitInput[counter].charAt(1) == 'e') ++counter;
-            } else {
+                if(splitInput[counter].length() == 2 && splitInput[counter].charAt(0) == '-' && splitInput[counter].charAt(1) == 'e') ++counter; //If the next substring in input it "-e", we assume that the pattern is coming
+            } else { //If the next substring in input it "-e", we assume that the pattern is coming
                 ++counter;
             }
         }
 
+        //The next substring should be the pattern
         params.pattern = splitInput[counter++];
 
+        //The remaining substrings are the files provided
         params.fileNames = new String[splitInput.length - counter];
         for(int i = 0; counter < splitInput.length; ++i, ++counter) {
             params.fileNames[i] = splitInput[counter];
@@ -47,10 +68,44 @@ public class InputParser {
         return AnswerGenerator.getResult(params);
     }
 
+    /**
+     * setFlags() sets the flags of params based on String s
+     * @param s - a String starting with '-' followed by
+     * @param params
+     * @throws InvalidFlagException
+     */
     static void setFlags(String s, InputParams params) throws InvalidFlagException {
-
+        for(int i = 1; i < s.length(); ++i) { //Checking only from index 1 to exclude '-'
+            switch(s.charAt(i)) {
+                case 'v' : {
+                    params.v = true;
+                } break;
+                case 'w' : {
+                    params.w = true;
+                } break;
+                case 'x' : {
+                    params.x = true;
+                } break;
+                case 'n' : {
+                    params.n = true;
+                } break;
+                case 'i' : {
+                    params.i = true;
+                } break;
+                case 'c' : {
+                    params.c = true;
+                } break;
+                default : {
+                    throw new InvalidFlagException("Invalid flag provided");
+                }
+            }
+        }
     }
 }
+
+/**
+ * InputParams stores the processed data required to evaluate the grep statement
+ */
 
 class InputParams {
     int num = 0;
@@ -62,6 +117,17 @@ class InputParams {
     boolean n = false;
     boolean i = false;
     boolean c = false;
+
+    @Override
+    public String toString() {
+            if(pattern == null) pattern = "[NULL]";
+            if(fileNames == null) fileNames = new String[] {"[NULL]"};
+            return "num : " + num + "\n" +
+                    "pattern : " + pattern + "\n" +
+                    "fileName 1 : " + fileNames[0] + "\n" +
+                    "v : " + v + " w : " + w + " x : " + x + " n : " + n + " i : " + i + " c : " + c + "\n";
+
+    }
 }
 
 class InvalidInputLayoutException extends Exception {
